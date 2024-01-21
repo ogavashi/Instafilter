39c5055a93a0aac410819822fb3a859134f5343c
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2024-01-21 13:04:49 +0200

    add load function

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var selectedItem: PhotosPickerItem?
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     
@@ -15,14 +17,16 @@ struct ContentView: View {
         NavigationStack {
             VStack {
                 Spacer()
-                
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
                 }
+                .onChange(of: selectedItem, loadImage)
                 
                 Spacer()
                 
@@ -48,6 +52,16 @@ struct ContentView: View {
     
     func changeFilter() {
         
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return
+            }
+            guard let inputImage = UIImage(data: imageData) else {
+                return
+            }
+        }
     }
 }
 
